910f4be6604c51afc73962a01b0213ddc5aecf18
Ry-leigh <ryanaarondc@gmail.com>
2026-02-11 18:54:27 +0800

    Done with Hello World Demo

---
+++ react-native-getting-started/ProfileDemo.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from "react-native"
+
+export default function Profile() {
+    return (
+        <View>
+            <Text>Course & Section: BS Iinformation System 3B</Text>
+            <Text>Name: Ryan</Text>
+            <Text>Age: 20</Text>
+            <Text>Favorite Hobby: Think Week</Text>
+            <Text>Pet Peeves: Shandi</Text>
+        </View>
+    )
+}
