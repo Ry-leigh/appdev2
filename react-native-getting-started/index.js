28ecee9ad562e032765f8c46bfc28ef5aeff743e
Ry-leigh <ryanaarondc@gmail.com>
2026-02-11 19:00:00 +0800

    Done with Profile Demo

--- react-native-getting-started/index.js
+++ react-native-getting-started/index.js
@@ -1,6 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 import App from './HelloWorldDemo';
+import Profile from './ProfileDemo';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
