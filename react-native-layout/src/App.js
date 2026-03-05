9f5ef7b6e9728cdcdd5fef986e48eee404014991
Ry-leigh <ryanaarondc@gmail.com>
2026-03-05 23:51:46 +0800

    Added Signup screen layout

--- react-native-layout/src/App.js
+++ react-native-layout/src/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screen/Login';
+import Signup from './screen/Signup';
 
 export default function App() {
   return (
-    <Login />
+    <Signup />
   );
 }
