2a6026ec39716e29748c702f54c8580b659860d2
Ry-leigh <ryanaarondc@gmail.com>
2026-03-05 23:41:01 +0800

    Done with flex wrap section

--- react-native-flexbox/src/App.js
+++ react-native-flexbox/src/App.js
@@ -30,22 +30,15 @@ const PreviewLayout = ({
   </View>
 );
 
-const AlignContentLayout = () => {
-  const [alignContent, setAlignContent] = useState('flex-start');
+const FlexWrapLayout = () => {
+  const [flexWrap, setFlexWrap] = useState('wrap');
 
   return (
     <PreviewLayout
-      label="alignContent"
-      selectedValue={alignContent}
-      values={[
-        'flex-start',
-        'flex-end',
-        'stretch',
-        'center',
-        'space-between',
-        'space-around',
-      ]}
-      setSelectedValue={setAlignContent}>
+      label="flexWrap"
+      selectedValue={flexWrap}
+      values={['wrap', 'nowrap']}
+      setSelectedValue={setFlexWrap}>
       <View style={[styles.box, {backgroundColor: 'orangered'}]} />
       <View style={[styles.box, {backgroundColor: 'orange'}]} />
       <View style={[styles.box, {backgroundColor: 'mediumseagreen'}]} />
@@ -60,7 +53,6 @@ const AlignContentLayout = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexWrap: 'wrap',
     marginTop: 8,
     backgroundColor: 'aliceblue',
     maxHeight: 400,
@@ -78,7 +70,6 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
     borderRadius: 4,
     backgroundColor: 'oldlace',
-    alignSelf: 'flex-start',
     marginHorizontal: '1%',
     marginBottom: 6,
     minWidth: '48%',
@@ -103,4 +94,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AlignContentLayout;
+export default FlexWrapLayout;
